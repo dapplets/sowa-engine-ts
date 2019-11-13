88ae58d20d1134d442aed1a55098eecc7b312300
Alexander Sakhaev <alsakhaev@gmail.com>
2019-11-13 15:11:27 +0300

    remove state from TxBuilder to fix test

--- src/interfaces/txBuilder.ts
+++ src/interfaces/txBuilder.ts
@@ -1,8 +1,5 @@
-import { State } from "../core/state";
-
 export interface TxBuilder {
     txConfig: any;
-    state: State;
     run(): Promise<any>; // async
     on(event: string, callback: Function): void; // for running of new state machine
 }
