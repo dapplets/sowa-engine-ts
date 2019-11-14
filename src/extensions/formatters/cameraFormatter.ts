0ab2bece1e3197ab906d123e4251025e02b27746
Alexander Sakhaev <alsakhaev@gmail.com>
2019-11-14 15:13:06 +0300

    private properites in features

--- src/extensions/formatters/cameraFormatter.ts
+++ src/extensions/formatters/cameraFormatter.ts
@@ -1,8 +1,6 @@
-// ToDo: it.
-
 export class CameraFormatter {
-    public static readonly globalName: string = "http://types.dapplets.org/ethereum/formatters/address/1.0";
-    public static readonly targetViewGlobalName: string = "http://types.dapplets.org/views/htmlMustache/1.0";
+    private static readonly globalName: string = "http://types.dapplets.org/ethereum/formatters/address/1.0";
+    private static readonly targetViewGlobalName: string = "http://types.dapplets.org/views/htmlMustache/1.0";
     public static readonly REG_KEY = [CameraFormatter.globalName, CameraFormatter.targetViewGlobalName];
 
     public txConfig: any;
--- src/extensions/formatters/ethAddressHtmlFormatter.ts
+++ src/extensions/formatters/ethAddressHtmlFormatter.ts
@@ -1,6 +1,6 @@
 export class EthAddressHtmlFormatter {
-    public static readonly globalName: string = "http://types.dapplets.org/ethereum/formatters/address/1.0";
-    public static readonly targetViewGlobalName: string = "http://types.dapplets.org/views/htmlMustache/1.0";
+    private static readonly globalName: string = "http://types.dapplets.org/ethereum/formatters/address/1.0";
+    private static readonly targetViewGlobalName: string = "http://types.dapplets.org/views/htmlMustache/1.0";
     public static readonly REG_KEY = [EthAddressHtmlFormatter.globalName, EthAddressHtmlFormatter.targetViewGlobalName];
 
     public txConfig: any;
