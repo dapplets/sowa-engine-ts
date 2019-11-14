4f0839a55bd2675abf3318e873f99186b91209d4
Alexander Sakhaev <alsakhaev@gmail.com>
2019-11-14 15:13:24 +0300

    less code

--- src/errors/incompatibleDappletError.ts
+++ src/errors/incompatibleDappletError.ts
@@ -1,8 +1,8 @@
+import { RegKey } from '../core/regKey';
+
 export class IncompatibleDappletError extends Error {
-    public readonly incompatibleFeatures?: string[][];
-    constructor(incompatibleFeatures?: string[][]) {
+    constructor(public readonly incompatibleFeatures?: RegKey[]) {
         super("Can not run incompatible dapplet.");
         this.name = "IncompatibleDappletError";
-        this.incompatibleFeatures = incompatibleFeatures;
     }
 }
