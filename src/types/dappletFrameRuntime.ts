9dde6e4482fe442ba7af772654e3c138f493d95f
Alexander Sakhaev <alsakhaev@gmail.com>
2019-11-15 10:46:44 +0300

    encode json with ethers in client side

--- src/types/dappletFrameRuntime.ts
+++ src/types/dappletFrameRuntime.ts
@@ -3,5 +3,5 @@ import { DappletRuntime } from './dappletRuntime';
 export type DappletFrameRuntime = {
     dappletId: string;
     dapplet: DappletRuntime;
-    txMeta: any;
+    txMeta?: Uint8Array;
 }
