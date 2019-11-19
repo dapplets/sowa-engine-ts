417579f0aaa161ef913b20b13bbdbcd009f7bf39
Dmitry Palchun <dima@ethernian.com>
2019-11-19 19:30:01 +0100

    [+] state machine loop (dirty)

--- src/core/dappletEngine.ts
+++ src/core/dappletEngine.ts
@@ -1,8 +1,12 @@
 import { DappletContext } from './dappletContext';
 import { DappletExecutable } from './dappletExecutable';
+const MIN_WAIT_FOR_NEXT_RUN_MLS = 1000
 
 export class DappletEngine {
 
+    isRunning: boolean=false;
+    isStateDirty: boolean=false;
+ 
     constructor(private _frameExecutables: DappletExecutable[], private _context: DappletContext) {
     }
 
@@ -19,20 +23,34 @@ export class DappletEngine {
     }
 
     public onApproved() {
+        //ToDo: do we need notifications from state or we can just recalculate all "when"?
+        this._frameExecutables.forEach(f => f.state.onUpdate(this.setStateIsDirty))
         run()
     }
 
     private run(): void {
-        //prepare transactions
-        const framePayloads = this._frameExecutables.map(f => f.prepare())
-        //pre-send processing and checks (like multisigs)
+        if (!this.isRunning && this.isStateDirty){
+            this.isRunning=true;
+            //prepare transactions
+            const framePayloads = this._frameExecutables.map(f => f.prepare())
+            //pre-send processing and checks (like multisigs)
 
-        //send all transactions
-        framePayloads.forEach(framePayload => {
-            framePayload.forEach((data,dappletId)=>{
-                //ToDo: create a subscribtion here
-                this._context.config.signers.get(name)!.signAndSend(data)
+            //ToDo: make overall payload checks 
+
+            //send all transactions
+            framePayloads.forEach(framePayload => {
+                framePayload.forEach((data,dappletId)=>{
+                    //ToDo: create a subscribtion here
+                    this._context.config.signers.get(name)!.signAndSend(data)
+                })
             })
-        })
+            this.isStateDirty = false;
+            this.isRunning=false;
+        }
+        setTimeout(this.run,MIN_WAIT_FOR_NEXT_RUN_MLS)
+    }
+
+    private setStateIsDirty() {
+        this.isStateDirty = true;
     }
 }
