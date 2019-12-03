dbb55842b86820000741baeb96b183f12620a9bd
Alexander Sakhaev <alsakhaev@gmail.com>
2019-12-03 16:35:24 +0300

    fix default config

--- src/defaultConfig.ts
+++ src/defaultConfig.ts
@@ -2,14 +2,12 @@ import { ContextConfig } from './types/contextConfig'
 import { GithubDappletProvider } from './providers/githubDappletProvider'
 import { CameraFormatter } from './extensions/formatters/cameraFormatter'
 import { EthereumExtension } from './extensions/ethereum'
-import { extensions } from '.'
+import { GridMustacheView } from './extensions/views/grid-mustache-view'
+import { PlainMustacheView } from './extensions/views/plain-mustache-view'
 
 const DEFAULT_CONFIG: ContextConfig = {
     providers: [new GithubDappletProvider()], // new SwarmDappletProvider()
-    views: [
-        extensions.views.gridMustache.GridMustacheView,
-        extensions.views.plainMustache.PlainMustacheView
-    ],
+    views: [GridMustacheView, PlainMustacheView],
     formatters: [CameraFormatter],
     extensions: [new EthereumExtension()]
     // ToDo: unify classes ([new X] vs [X])
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { DappletContext } from './core/dappletContext'
+import { Renderable } from './interfaces/renderable'
+import { Signable } from './interfaces/signable'
 import * as extensions from './extensions'
 
 export {
     DappletContext,
+    Renderable,
+    Signable,
     extensions
 }
